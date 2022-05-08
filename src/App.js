723c3c9e1f90730f22ac5f120953ee5d686c0081
Keilla <keillavfb@gmail.com>
2022-05-08 14:42:42 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends React.Component {
 
-export default App;
+  state = {
+    title: "",
+    summary: "",
+    genre: "",
+    authorName: "",
+    pages: 0,
+    yearOfPublication: 0
+  }
+
+  create = () => {
+    alert("Livro cadastrado com sucesso!");
+  }
+
+  render() {
+    return (
+      <div className="App">
+          <div className='cabecalho'>
+            <h1 class="cabecalho">Cadastrar novo livro</h1>
+          </div>
+          <form action='' className='formulario'>
+            <div>
+                <label>Título: </label>
+                <input className='box' type="text" value={this.state.title} onChange={(e) => {this.setState({title: e.target.value})}} />
+                <label>Sinopse: </label>
+                <input className='box' type="text" value={this.state.summary} onChange={(e) => {this.setState({summary: e.target.value})}} />
+                <label>Gênero: </label>
+                <input className='box' type="text" value={this.state.genre} onChange={(e) => {this.setState({genre: e.target.value})}} />
+                <label>Nome do autor: </label>              
+                <input className='box' type="text" value={this.state.authorName} onChange={(e) => {this.setState({authorName: e.target.value})}} />
+                <label>Páginas: </label>
+                <input className='box-number' type="number" value={this.state.pages} onChange={(e) => {this.setState({pages: e.target.value})}} />
+                <label>Ano de publicação: </label>
+                <input className='box-number' type="number" value={this.state.yearOfPublication} onChange={(e) => {this.setState({yearOfPublication: e.target.value})}} />
+              </div>
+              <input className="botao" type="button" value="Cadastrar" onClick={this.create}/>
+          </form>
+      </div>
+    );
+  }
+}
